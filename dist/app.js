0189032fbbf54fd5611c99995451b511ae86da5c
tanaliga100 <jordantanaliga@gmail.com>
2023-06-04 14:49:46 +0800

    apply-security-packages

--- dist/app.js
+++ dist/app.js
@@ -12,8 +12,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
+const helmet_1 = __importDefault(require("helmet"));
 const morgan_1 = __importDefault(require("morgan"));
 const path_1 = __importDefault(require("path"));
 const connectDB_1 = require("./config/connectDB");
@@ -30,6 +32,9 @@ app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, morgan_1.default)("dev"));
 app.use(express_1.default.static(path_1.default.join(__dirname, "public")));
 // SECURITY PACKAGES
+app.use((0, helmet_1.default)());
+app.use((0, cors_1.default)());
+app.use();
 // ROUTES
 app.get("/", (req, res) => {
     res.send(`
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
+import cors from "cors";
 import dotenv from "dotenv";
 import { Express, Request, Response, default as express } from "express";
+import { rateLimit } from "express-rate-limit";
+import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
+import xss from "xss-clean";
 import { connectDB } from "./config/connectDB";
 import authenticationMiddleware from "./middlewares/auth.middleware";
 import { errorHandlerMidlleware } from "./middlewares/errorHandler.middleware";
@@ -9,7 +13,6 @@ import { notFoundMiddleware } from "./middlewares/notFound.middleware";
 import { AuthRoute } from "./routes/auth.routes";
 import { JobsRoute } from "./routes/jobs.routes";
 import { UserRoute } from "./routes/user.routes";
-
 dotenv.config();
 
 const app: Express = express();
@@ -17,7 +20,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
+
 // SECURITY PACKAGES
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.set("trust proxy", 1);
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  })
+);
+
 // ROUTES
 app.get("/", (req: Request, res: Response) => {
   res.send(`
